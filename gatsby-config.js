dea773dc17593170877bbd211ae94b36955781b7
Dismalness <domainexpansion@protonmaiL.com>
2021-09-01 13:06:12 +0100

    Added a temporary Coming Soon page with an a counter towards IEEE Day

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: 'IEEE INSAT STUDENT BRANCH 2021-2022',
+    description: 'IEEE INSAT STUDENT BRANCH Website 2021-2022',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+  ],
+}
