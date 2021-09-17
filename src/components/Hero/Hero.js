707a21fc3b5428fe7d6de96727bb93226678065b
Dismalness <domainexpansion@protonmaiL.com>
2021-09-17 15:20:29 +0100

    Added hero section with video
    Former-commit-id: 5c5519810700c42c317706d1a8c1aa30043fce91

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Video from "../../assets/videos/video.mp4";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from "./HeroStyle";
+import { Button } from "../ButtonStyle";
+
+const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+
+  return (
+    <HeroContainer id="home">
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>INSAT IEEE Student Branch</HeroH1>
+        <HeroP>
+          One Dream, One Team!
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to="/"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact="true"
+            offset={-80}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
