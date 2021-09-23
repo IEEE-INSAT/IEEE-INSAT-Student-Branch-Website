6adb9676de66a720a403bcc108f8f3d04684829e
Zexus <zexus@pop-os.localdomain>
2021-09-23 18:42:04 +0100

    fixed the polygon clip path of carousel images and their dimensions
    Former-commit-id: 5f64398da86ccb9fc7a0b4bc42512bb116b2718c

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -22,7 +22,7 @@ const Activities = () => {
           <InfoContainer>
             <ActivityTitle>Title</ActivityTitle>
             <ActivityDescription>
-              tetatatatatatatatatatatatatatatatatatatatat
+              it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop 
             </ActivityDescription>
           </InfoContainer>
         </Card>
@@ -33,7 +33,7 @@ const Activities = () => {
           <InfoContainer>
             <ActivityTitle>Title</ActivityTitle>
             <ActivityDescription>
-              tetatatatatatatatatatatatatatatatatatatatat
+              it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop 
             </ActivityDescription>
           </InfoContainer>
         </Card>
@@ -44,7 +44,7 @@ const Activities = () => {
           <InfoContainer>
             <ActivityTitle>Title</ActivityTitle>
             <ActivityDescription>
-              tetatatatatatatatatatatatatatatatatatatatat
+              it is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop 
             </ActivityDescription>
           </InfoContainer>
         </Card>
