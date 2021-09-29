7ccd459ed94d1aa157e52ed9a731b20804868656
Dismalness <domainexpansion@protonmail.com>
2021-09-29 10:28:56 +0100

    updated responsiveness
    Former-commit-id: 84568c648b1c72b0800d22f46fa1326af32a2869 [formerly 2db00bd3fcf0b28ac7d559d149875136489bb056]
    Former-commit-id: 918fc0d4cc60a05c2d86a5ba32d725f68a7fdacc

--- src/components/Chapters/ChapterStyle.js
+++ src/components/Chapters/ChapterStyle.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ChaptersContainer = styled.div`
-  height: 860px;
+  height: 900px;
   display: flex;
   flex-direction: column;
   justify-content: center;
