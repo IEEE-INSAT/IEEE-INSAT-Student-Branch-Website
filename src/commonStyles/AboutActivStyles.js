d7155b7e9ccd087d48c2ab82b27476787a7af65b
Dismalness <domainexpansion@protonmail.com>
2021-11-18 11:07:17 +0100

    new vid

--- src/commonStyles/AboutActivStyles.js
+++ src/commonStyles/AboutActivStyles.js
@@ -32,13 +32,11 @@ export const InfoRow = styled.div`
   }
 `;
 export const Column1 = styled.div`
-  margin-bottom: 15px;
-  padding: 0 25px;
+  
   grid-area: col1;
 `;
 export const Column2 = styled.div`
-  margin-bottom: 15px;
-  padding: 0 15px;
+  
   grid-area: col2;
 `;
 export const TextWrapper = styled.div`
