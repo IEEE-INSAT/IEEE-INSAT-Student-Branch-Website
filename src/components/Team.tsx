d23f2423998fb93a0ea0d4ce7644c8f1c1aaa70e
Mahdi Chaari <mahdi.chaari2@insat.u-carthage.tn>
2023-12-09 01:01:33 +0100

    added image optimizations to Team Component
    Former-commit-id: 88ffbc011d93d03a00be3e360ce6be32fd07332d [formerly 890f899883afa0ba3a4dc68694c68ffaf2cf3f66] [formerly 3be19bc6f54a479ac43d684adbe12dc83f0f3812 [formerly de82b3e6da945083ff4ce7b66cc248bfa6d6d6bc]]
    Former-commit-id: a04d16c56b872e0b37851dacf0c939c3d0e35a85 [formerly 554e7f5aed9a3ede102946eec14ff60fd2249b2d]
    Former-commit-id: 390716c67c3d65b767fdefcfa4173c14ac98e7d2

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,60 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
+import { AdvancedImage } from "@cloudinary/react";
+import { cld } from "../cloudinary";
+import { fit } from "@cloudinary/url-gen/actions/resize";
 
 const team: [string, string, string][] = [
-  [
-    "Mahdi Chaari",
-    "Chairman",
-    "https://res.cloudinary.com/dzgxf5tsm/image/upload/v1701454793/xykmv80ccatoi8829hh6.jpg",
-  ],
-  [
-    "Adam Dey",
-    "Vice Chair Technical Activities",
-    "https://res.cloudinary.com/dzgxf5tsm/image/upload/v1701454769/nzthnxyehndaic0xaaiu.jpg",
-  ],
-  [
-    "Faten Ben Rejeb",
-    "Vice Chair Membership",
-    "https://res.cloudinary.com/dzgxf5tsm/image/upload/v1701458566/Board/aqth4yoe4ldtff7pytyb.jpg",
-  ],
-  [
-    "Wissal Khaled",
-    "General Secretary",
-    "https://res.cloudinary.com/dzgxf5tsm/image/upload/v1701454767/nnxev0gvbylinyaf3e1f.jpg",
-  ],
-  [
-    "Ghada Daghmoura",
-    "Treasurer",
-    "https://res.cloudinary.com/dzgxf5tsm/image/upload/v1701461978/kzkwcyftdtjxg4s2uiyk.jpg",
-  ],
-  [
-    "Mohammed Hassan Amara",
-    "Webmaster",
-    "https://res.cloudinary.com/dzgxf5tsm/image/upload/v1701454773/ibmo5mmxplv99nc2q5xj.jpg",
-  ],
-  [
-    "Nermine Moumen",
-    "Community Manager",
-    "https://res.cloudinary.com/dzgxf5tsm/image/upload/v1701462979/uenoslkcacufuz0u62mk.jpg",
-  ],
-  [
-    "Omar Idoudi",
-    "Graphic Designer",
-    "https://res.cloudinary.com/dzgxf5tsm/image/upload/v1701455231/vbt8ja5u1yil2afnmsdl.jpg",
-  ],
-  [
-    "Rayen Hamza",
-    "Photographer",
-    "https://res.cloudinary.com/dzgxf5tsm/image/upload/v1701455320/mubrnlodbkvbsdkkwzrj.jpg",
-  ],
+  ["Mahdi Chaari", "Chairman", "xykmv80ccatoi8829hh6"],
+  ["Adam Dey", "Vice Chair Technical Activities", "nzthnxyehndaic0xaaiu"],
+  ["Faten Ben Rejeb", "Vice Chair Membership", "Board/aqth4yoe4ldtff7pytyb"],
+  ["Wissal Khaled", "General Secretary", "nnxev0gvbylinyaf3e1f"],
+  ["Ghada Daghmoura", "Treasurer", "kzkwcyftdtjxg4s2uiyk"],
+  ["Mohammed Hassan Amara", "Webmaster", "ibmo5mmxplv99nc2q5xj"],
+  ["Nermine Moumen", "Community Manager", "uenoslkcacufuz0u62mk"],
+  ["Omar Idoudi", "Graphic Designer", "vbt8ja5u1yil2afnmsdl"],
+  ["Rayen Hamza", "Photographer", "mubrnlodbkvbsdkkwzrj"],
 ];
 
 function TeamMember(props: { src: string; name: string; role: string }) {
   return (
     <div className="text-center">
-      <img
-        className="rounded-full w-24 h-24 mx-auto object-cover"
-        src={props.src}
-        alt={props.name}
+      <AdvancedImage
+        cldImg={cld.image(props.src).resize(fit().width(200).height(200))}
+        className="object-cover rounded-full overflow-hidden w-24 h-24 mx-auto"
       />
       <div className="mt-2 sm:mt-4">
         <h3 className="font-medium text-gray-800 dark:text-gray-200">
