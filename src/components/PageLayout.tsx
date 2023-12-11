376c0f6ea67684efab91e65e2e82662027590f12
Mahdi Chaari <mahdi.chaari2@insat.u-carthage.tn>
2023-12-11 20:02:01 +0100

    added star bg

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,14 +1,32 @@
-import {PropsWithChildren} from "react";
+import { PropsWithChildren } from "react";
 import Header from "./header";
 import Footer from "./footer";
+import "./Stars.css";
 
 export default function PageLayout(props: PropsWithChildren) {
-    return <>
-        <main id="content" role="main" className={"w-full"} style={{contain:"paint"}}>
-            <div className=""><Header/>{props.children}
-            </div>
-        </main>
-        <Footer/></>
-
-
+  return (
+    <>
+      <div
+        className={
+          "h-full absolute top-0 w-full opacity-50 mix-blend-luminosity"
+        }
+      >
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+      </div>
+      <main
+        id="content"
+        role="main"
+        className={"w-full"}
+        style={{ contain: "paint" }}
+      >
+        <div className="">
+          <Header />
+          {props.children}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
